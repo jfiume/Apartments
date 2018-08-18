4bde59f7c4800ccd22df0f8c638cd728edfe1c0c
Joe Fiume <joefiume88@gmail.com>
2018-08-17 18:08:50 -0700

    added apartment reducer

---
+++ app/javascript/packs/reducers/apartment_reducer.js
@@ -0,0 +1,17 @@
+import {
+  RECEIVE_APARTMENT,
+  RECEIVE_APARTMENTS
+} from '../actions/apartment_actions';
+
+const ApartmentReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_APARTMENT:
+      const newApartment = action.apartment;
+      return Object.assign({}, newApartment);
+    case RECEIVE_APARTMENTS:
+      return Object.assign({}, action.apartments);
+    default:
+      return state;
+  }
+};
